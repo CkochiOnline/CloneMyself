f974f4cbb7dd228bf08ab9589da4eb0b9b3fc7aa
CkochiOnline <ckochionline@gmail.com>
2024-11-22 13:45:51 +0100

    Add project files.

---
+++ CloneMyself/NewClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneMyself
+{
+    internal class NewClass
+    {
+        string name;
+        string abilities;
+        string usingAbilites;
+
+        public NewClass(string name, string abilities, string usingAbilites)
+        {
+            this.name = name;
+            this.abilities = abilities;
+            this.usingAbilites = usingAbilites;
+        }
+
+        public void PrintText()
+        {
+            Console.WriteLine($"Jeg heter {name}, mine egenskaper er {abilities} og jeg bruker dem til {usingAbilites}");
+        }
+    }
+}
---
+++ CloneMyself/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CloneMyself
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Hva heter du?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hva er egenskapene dine?");
+            var abilities = Console.ReadLine();
+
+            Console.WriteLine("Hva bruker du egenskapene til?");
+            var usingAbilities = Console.ReadLine();
+
+            NewClass Christoffer = new NewClass(name, abilities, usingAbilities);
+
+            Christoffer.PrintText();       
+        }
+    }
+}
